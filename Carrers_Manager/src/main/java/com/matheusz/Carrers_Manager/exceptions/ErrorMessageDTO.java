e088861963f68c768a1c8db161c20240588dba4d
M-Zweibrucker <matheuszweibrucker@gmail.com>
2024-09-01 20:32:19 -0300

    feat: exception handler

---
+++ Carrers_Manager/src/main/java/com/matheusz/Carrers_Manager/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package com.matheusz.Carrers_Manager.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
