a22f3ea46a8f0fbee1afe40fb5096ed05f4c190a
M-Zweibrucker <matheuszweibrucker@gmail.com>
2024-09-01 20:29:28 -0300

    Fisrt commit

---
+++ Carrers_Manager/src/main/java/com/matheusz/Carrers_Manager/CarrersManagerApplication.java
@@ -0,0 +1,13 @@
+package com.matheusz.Carrers_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrersManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrersManagerApplication.class, args);
+	}
+
+}
