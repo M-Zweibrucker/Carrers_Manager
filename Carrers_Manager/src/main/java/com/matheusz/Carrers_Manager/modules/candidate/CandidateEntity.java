2d334e6348bdfdc879321404af4ee26740514010
M-Zweibrucker <matheuszweibrucker@gmail.com>
2024-09-02 15:44:39 -0300

    feat: candidate entity and repo

--- Carrers_Manager/src/main/java/com/matheusz/Carrers_Manager/modules/candidate/CandidateEntity.java
+++ Carrers_Manager/src/main/java/com/matheusz/Carrers_Manager/modules/candidate/CandidateEntity.java
@@ -1,22 +1,31 @@
 package com.matheusz.Carrers_Manager.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
     @NotBlank
     @Pattern(regexp = "\\S+", message = "The user name field must not have blank spaces")
-    private String userName;
+    private String username;
 
     @Email(message = "The email is not valid")
     private String email;
@@ -25,4 +34,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
 }
