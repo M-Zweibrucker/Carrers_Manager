7678dd7285d9024b17de6226390d52e54743d686
M-Zweibrucker <matheuszweibrucker@gmail.com>
2024-09-02 15:41:59 -0300

    feat: candidate controller

--- Carrers_Manager/src/main/java/com/matheusz/Carrers_Manager/modules/candidate/controllers/CandidateController.java
+++ Carrers_Manager/src/main/java/com/matheusz/Carrers_Manager/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,10 @@
 package com.matheusz.Carrers_Manager.modules.candidate.controllers;
 
+import com.matheusz.Carrers_Manager.exceptions.UserFoundException;
 import com.matheusz.Carrers_Manager.modules.candidate.CandidateEntity;
+import com.matheusz.Carrers_Manager.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity){
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
+
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        return this.candidateRepository.save(candidateEntity);
     }
 }
